752a8810b16d06546078440c4fd1316073a45dbb
Luciano Ilha <ilha.dev.br@gmail.com>
2021-09-21 16:47:22 -0300

    Add cart modal.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Prods from "./components/Prods/Prods";
 
 function App() {
   return (
     <>
+      <Cart />
       <Header />
       <main>
         <Prods />
